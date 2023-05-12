4541466ce9dd36585365f25f4a4e4c025049dda3
Юлия <YUGG2512@gmail.com>
2023-05-12 17:05:46 +0300

    max из трёх

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает три числа и выдает max из этих чисел 
+
+Console.WriteLine("Введите число а");
+Console.WriteLine("Введите число b");
+Console.WriteLine("Введите число c");
+
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
