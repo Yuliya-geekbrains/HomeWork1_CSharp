e7e33928f279027112123e3451a0727d8f884d6d
Юлия <YUGG2512@gmail.com>
2023-05-12 17:25:53 +0300

    odd number

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу, которая на вход принимает число и выдает, является ли число чётным 
+//(делится ли она на 2 без остатка)
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{Console.WriteLine(a);}
+else
+{Console.WriteLine("Число нечетное");}
+
+
